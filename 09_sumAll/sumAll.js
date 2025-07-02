bdae44e71730e047187b51171526390f216f7a30
Fjurn01 <126229845+Fjurn01@users.noreply.github.com>
2025-07-02 11:43:58 +0300

    Exercises 1-9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(one, two) {
+    let first,second;
+    if(one < two) {
+         first = one 
+         second = two;
+    }
+    else if(one > two) {
+         first = two 
+         second = one;
+        }
+    if(first <= 0 || second <= 0)
+    {
+        return "ERROR";
+    }
+    if( !Number.isInteger(one)  || !Number.isInteger(two))
+                return "ERROR";
+    let sum =0;
+    while(first <= second)
+    {
+        sum+= first;
+        first++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
