112b12abd9d8c73cd52284d61810c9800795610c
Fjurn01 <126229845+Fjurn01@users.noreply.github.com>
2025-07-02 11:50:58 +0300

    exercises 1-8

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let countLetters =0;
+    for(let letter in string)
+        countLetters++;
+    countLetters--; // accounts for the /0
+    let queue = "";
+    for(let letter in string)
+    {
+        queue += string.charAt(countLetters);
+        --countLetters;
+    }
+    return queue;
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...others) {
+    let object = [];
+
+    for(let number of array)
+    {
+        if(!others.includes(number))
+            object.push(number);
+    }
+    return object;
+
 };
 
 // Do not edit below this line
